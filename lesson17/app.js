eb7aee4cf8f370c6c09455002e1c1f5c199ef9a9
liaoqiangang <liaoqiangang@outlook.com>
2020-01-14 16:16:48 +0800

    lesson14-17

---
+++ lesson17/app.js
@@ -0,0 +1,17 @@
+const one = new Vue({
+  el: "#vue-app-one",
+  data: {
+    title: "this is app one"
+  },
+  methods: {},
+  computed: {}
+});
+
+const two = new Vue({
+  el: "#vue-app-two",
+  data: {
+    title: "this is app two"
+  },
+  methods: {},
+  computed: {}
+});
